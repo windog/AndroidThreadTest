334d2c5a0161a30ad33a64d8245471495b7251c1
windog <windiscall@gmail.com>
2016-04-19 10:40:56 +0800

    ..

--- app/src/main/java/com/windog/androidthreadtest/MainActivity.java
+++ app/src/main/java/com/windog/androidthreadtest/MainActivity.java
@@ -1,13 +1,47 @@
 package com.windog.androidthreadtest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    TextView text;
+    Button btnChangeText;
+
+    public static final int UPDATE_TEXT = 1;
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case UPDATE_TEXT:
+                    text.setText("Nice to meet you!");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    };
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         text = (TextView) findViewById(R.id.text);
+
+        btnChangeText = (Button) findViewById(R.id.btnChangeText);
+        btnChangeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Message message = new Message();
+                message.what = UPDATE_TEXT;
+                handler.sendMessage(message);
+            }
+        });
     }
 }
